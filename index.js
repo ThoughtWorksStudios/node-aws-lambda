3b662c163fd8e1fbc89645f91392850e7c2fd2f3
xaralis <xaralis@centrum.cz>
2015-11-30 13:30:24 +0100

    Replace usage of undefined `gutil.log` by `logger`

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ exports.deploy = function(codePackage, config, callback, logger, lambda) {
         if (err) {
           var warning = 'Package upload failed. '
           warning += 'Check your iam:PassRole permissions.'
-          gutil.log(warning);
+          logger(warning);
           callback(err)
         } else {
           lambda.updateFunctionConfiguration(params, function(err, data) {
